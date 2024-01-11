87adc024a59ef724c959acdc236fa7e8d3431b47
Kenichi Maeda <Kenichi.Maeda001@umb.edu>
2024-01-10 21:22:28 -0500

    change5

--- EXP/ChatGPT/encode.py
+++ EXP/ChatGPT/encode.py
@@ -1,5 +1,6 @@
 import base64
 from io import BytesIO
+import cv2
 
 import sys
 sys.path.append('../EXP/')
@@ -21,15 +22,19 @@ class Encode:
         image_buffer.close()
 
         return base64_string
-        """
+    
+    """
+        # Convert directly to jpeg data
+    
+        data, labels, parameters = C.Figure12.generate_datapoint()
         image = C.Figure12.data_to_image(data)
         plt.axis('off')
         plt.imshow(image)
 
-        image_buffer = BytesIO()
-        plt.savefig(image_buffer, format='png', dpi=200)
-        image_buffer.seek(0)
-        base64_string = base64.b64encode(image_buffer.read()).decode('utf-8')
-        image_buffer.close()
+        _, JPEG = cv2.imencode('.jpeg', frame)
+        jpeg_data = JPEG.tobytes()
 
-        return base64_string"""
+        base64_string = base64.b64encode(jpeg_data).decode('utf-8')
+
+        return base64_string
+    """
