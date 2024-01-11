2b4d62b22e4695f3d37a15076d897809c8934cd7
Kenichi Maeda <Kenichi.Maeda001@umb.edu>
2024-01-10 21:17:47 -0500

    change4

--- EXP/ChatGPT/encode.py
+++ EXP/ChatGPT/encode.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 
 class Encode:
     @staticmethod
-    def encode_image(data):
+    def encode_image():
+        data, labels, parameters = C.Figure12.generate_datapoint()
         image = C.Figure12.data_to_image(data)
         plt.axis('off')
         plt.imshow(image)
@@ -20,3 +21,15 @@ class Encode:
         image_buffer.close()
 
         return base64_string
+        """
+        image = C.Figure12.data_to_image(data)
+        plt.axis('off')
+        plt.imshow(image)
+
+        image_buffer = BytesIO()
+        plt.savefig(image_buffer, format='png', dpi=200)
+        image_buffer.seek(0)
+        base64_string = base64.b64encode(image_buffer.read()).decode('utf-8')
+        image_buffer.close()
+
+        return base64_string"""
--- EXP/ChatGPT/query.py
+++ EXP/ChatGPT/query.py
@@ -28,7 +28,7 @@ class Query:
                 {
                 "type": "image_url",
                 "image_url": {
-                    "url": f"data:image/png;base64,{base64_image}"
+                    "url": f"data:image/jpeg;base64,{base64_image}"
                 }
                 }
             ]
